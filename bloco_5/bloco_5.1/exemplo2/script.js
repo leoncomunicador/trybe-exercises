f7344d005f2060117a048630fa6f8ce7009a72af
Daniel Leon <daniel.radialista@gmail.com>
2021-05-18 15:17:06 -0300

    estudando bloco 5.1

---
+++ bloco_5/bloco_5.1/exemplo2/script.js
@@ -0,0 +1,12 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById("page-title").innerHTML = "Filme Favorito: Homens de Honra";
+
+document.getElementById("second-paragraph").innerText = "Praticando o DOM para me auxiliar nos blocos seguintes do JS."
+
+document.getElementById("subtitle").innerText = "Bloco 5 - JavaScript: DOM, Eventos e WebStorage"
+
+document.getElementsByClassName("p-teste")[0].style.color = "blue";
+
+document.getElementsByTagName("h4")[0].innerHTML = "Primeiros passos no DOM";
---
+++ bloco_5/bloco_5.1/exemplo4.js/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".pilotos-sem-titulo p");
+// <p class=​"piloto-f1-atual">​Lando Norris​</p>​
+document.querySelector(".piloto-f1-atual");
+// <p class=​"piloto-f1-atual">​Lewis Hamilton​</p>​
+document.querySelector(".pilotos-sem-titulo .piloto-f1-atual");
+// <div class=​"piloto-f1-atual">​George Russel​</div>

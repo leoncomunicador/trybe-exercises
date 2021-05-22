5b98ab63ffacea3d46aa19af5a0ccaaf15c1a95d
Daniel Leon <daniel.radialista@gmail.com>
2021-05-22 19:05:56 -0300

    exercícios 5.2 prontos

---
+++ bloco_5/bloco_5.1/exercicio/script.js
@@ -0,0 +1,13 @@
+document.querySelector("#header-container").style.backgroundColor = "green";
+
+document.querySelector(".emergency-tasks").style.backgroundColor = "##FFB6C1";
+
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "purple";
+
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "purple";
+
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "yellow";
+
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "black";
+
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "black";
